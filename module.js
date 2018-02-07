c4eed80ff3327f0c4da1a6e7f55ebf979719842a
Unknown <paul86677889@gmail.com>
2018-02-07 14:35:58 +0800

    AjaxTestGood

--- module.js
+++ module.js
@@ -189,10 +189,7 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var ModuleName = 'calendar';
-var ModuleDefaults = {
-    //
-
-};
+var ModuleDefaults = {};
 var ModuleReturns = ['output', 'methods'];
 
 var Module = function () {
@@ -210,6 +207,11 @@ var Module = function () {
             var self = this;
             var $this = this.$ele;
             var opts = this.option;
+
+            console.log('moduleIn!!!!');
+            this.creatHtml();
+            // this.getAjax();
+
             return this;
         }
     }, {
@@ -220,6 +222,84 @@ var Module = function () {
     }, {
         key: 'calendar',
         value: function calendar() {
+            return this;
+        }
+    }, {
+        key: 'creatHtml',
+        value: function creatHtml() {
+            var self = this;
+            var $this = this.$ele; //class="calendar"
+            var calendarHtml = '<div class="calendars_tabWrap">' + '<a href="#" class="prev on">' + '</a>' + '<ul class="ntb_tab">' + '<li class="tab">' + '<a href="#">' + '<span>' + '2017 7月' + '</span>' + '</a>' + '</li>' + '<li class="tab">' + '<a href="#">' + '<span>' + '2017 8月' + '</span>' + '</a>' + '</li>' + '<li class="tab">' + '<a href="#">' + '<span>' + '2017 9月' + '</span>' + '</a>' + '</li>' + '</ul>' + '<a href="#" class="next on">' + '</a>' + '</div>' + '<div class="calendars_weeksWrap">' + '<th>' + '星期日' + '</th>' + '<th>' + '星期一' + '</th>' + '<th>' + '星期二' + '</th>' + '<th>' + '星期三' + '</th>' + '<th>' + '星期四' + '</th>' + '<th>' + '星期五' + '</th>' + '<th>' + '星期六' + '</th>' + '</div>' + '<ul class="calendars_daysWrap">' + '<li class="calendars_days disabled">' + '</li>' + '<li class="calendars_days hasData">' + '<div class="date">' + '<span class="num">' + '1' + '</span>' + '<span class="weekday">' + '星期四' + '</span>' + '</div>' + '<span class="status">' + '候補' + '</span>' + '<span class="sell">' + '可賣：0' + '</span>' + '<span class="group">' + '團位：0' + '</span>' + '<span class="tip">' + '<i class="ic-ln productreferf">' + '</i>' + '保證出團' + '</span>' + '<span class="price">' + '$4,999' + '</span>' + '</li>' + '<li class="calendars_days hasData">' + '<div class="date">' + '<span class="num">' + '1' + '</span>' + '<span class="weekday">' + '星期五' + '</span>' + '</div>' + '<span class="status">' + '候補' + '</span>' + '<span class="sell">' + '可賣：0' + '</span>' + '<span class="group">' + '團位：0' + '</span>' + '<span class="tip">' + '<i class="ic-ln productreferf">' + '</i>' + '保證出團' + '</span>' + '<span class="price">' + '$4,999' + '</span>' + '</li>' + '</ul>';
+
+            $this.append(calendarHtml);
+            return this;
+        }
+    }, {
+        key: 'getAjax',
+        value: function getAjax() {
+            $.ajax({
+                dataType: "json",
+                method: 'GET',
+                url: './json/data4.json'
+            }).done(function (dataSource) {
+                // alert(dataSource[0]);
+                console.log(dataSource);
+                var NumOfJData = dataSource.length;
+                console.log(NumOfJData);
+                var i = 0;
+                $.each(dataSource, function () {
+                    $("#JSON_table").append("<tr>" + "<td>" + dataSource[i].date + "</td>" + "<td>" + dataSource[i].price + "</td>" + "<td>" + dataSource[i].status + "</td>" + "<td><p>總人數:" + dataSource[i].totalVacnacy + "</p></td>" + "<td><p>剩餘人數:" + dataSource[i].availableVancancy + "</p></td>" + "</tr>");
+                    i++;
+                });
+            });
+            return this;
+        }
+
+        // 下一個有資料的月份
+
+    }, {
+        key: 'nextMonth',
+        value: function nextMonth() {
+            return this;
+        }
+
+        // 上一個有資料的月份
+
+    }, {
+        key: 'prevMonth',
+        value: function prevMonth() {
+            return this;
+        }
+
+        // 切換日曆或列表模式
+
+    }, {
+        key: 'switch',
+        value: function _switch() {
+            return this;
+        }
+
+        // 加資料時如果有相同日期的資料，以後輸入為主，輸入時如果輸入沒有的月份，模組會加上該月份
+
+    }, {
+        key: 'inputData',
+        value: function inputData() {
+            return this;
+        }
+
+        // 重設資料時，月曆、tab重新產出
+
+    }, {
+        key: 'resetData',
+        value: function resetData() {
+            return this;
+        }
+
+        // destroy calendar，destroy時連class new出來的實例物件也要刪除
+
+    }, {
+        key: 'destroy',
+        value: function destroy() {
             return this;
         }
     }]);
