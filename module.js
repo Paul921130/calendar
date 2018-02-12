39e80ce6529ffade3c3d5702807c5c012f28e44d
Unknown <paul86677889@gmail.com>
2018-02-12 17:04:17 +0800

    change

--- module.js
+++ module.js
@@ -336,34 +336,38 @@ var Module = function () {
             //小箭頭跳currentMonth
             $('.next').on('click', function () {
                 var goMonth = parseInt($('.currentMonth').attr('data-label')) - 201801;
-
                 if ($(".tab:nth-child(3) a").hasClass('currentMonth') === false) {
                     $(".tab a").removeClass('currentMonth');
-                    $(".tab a").attr('id', '');
-                    $(".tab:nth-child(" + (goMonth + 1) + ") a").attr('id', 'currentMonth');
+                    // $(".tab a").attr('id','');
+                    // $(".tab:nth-child("+(goMonth+1)+") a").attr('id','currentMonth');
                     $(".tab:nth-child(" + (goMonth + 1) + ") a").addClass('currentMonth');
                     console.log('gogo!' + goMonth);
+                    self.monthSelect();
+                    self.getNowMonth();
+                    self.bornCalendar();
                     // goMonth++;
-                    goMonth++;
+                    // goMonth++;
                 } else {
                     var goMonth = parseInt($('.currentMonth').attr('data-label')) - 201801;
                     $this.find('.ntb_tab').empty();
-                    $(".tab a").removeClass('currentMonth');
                     for (var i = goMonth; i <= goMonth + 2; i++) {
                         var nextMonthMo = moment().add(i, 'months').format("YYYY MMM");
                         console.log(nextMonthMo);
                         var monthsTitle = '<li class="tab">' + '<a href="#" class="' + moment().add(i, 'months').format("YYYYMM") + '" id="" data-label="' + moment().add(i, 'months').format("YYYYMM") + '">' + '<span>' + nextMonthMo + '</span>' + '</a>' + '</li>';
                         $this.find('.ntb_tab').append(monthsTitle);
                         $(".tab a").attr('id', '');
+                        $(".tab:nth-child(1) a").attr('id', 'currentMonth');
                         $(".tab:nth-child(1) a").addClass('currentMonth');
                     };
+                    self.monthSelect();
+                    self.getNowMonth();
+                    self.bornCalendar();
                     return this;
                 };
-                self.monthSelect();
-                self.getNowMonth();
-                self.bornCalendar();
                 console.log($('.currentMonth').attr('data-label'));
             }); //小箭頭跳currentMonth
+
+
             $('.prev').on('click', function () {
                 var goMonth = parseInt($('.currentMonth').attr('data-label')) - 201801;
 
@@ -383,12 +387,6 @@ var Module = function () {
             }); //小箭頭跳currentMonth        
 
             // 小箭頭跳currentMonth
-
-            // $('.on').on('click',function(){
-            //     console.log('hi');
-            //     var ssss= $(".currentMonth").parents(".tab").index();
-            //     console.log("索引是"+ssss);
-            // });
 
 
             // 小箭頭跳頁click
